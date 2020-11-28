8c364ee9cc7342f330f7cd3a95ad361c2e4bed9d
Filip Oščádal <f@mxd.cz>
2020-11-28 20:45:13 +0100

    web sync

--- create_pdf.sh
+++ create_pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-command -v docker >/dev/null 2>&1 || {echo "Docker is NOT installed!";exit;}
+command -v docker >/dev/null 2>&1 || { echo "Docker is NOT installed!";exit; }
 
 find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.md" -exec echo "{}" \; -exec docker run --rm -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o "{}.adoc" \;
 find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.adoc" -exec echo "{}" \; -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -a allow-uri-read -d book "{}" \;
