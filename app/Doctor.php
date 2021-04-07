afacbbda4f703fedbc80c021278040d80997d2a9
Filip Oščádal <f@mxd.cz>
2021-04-07 20:31:45 +0200

    robotic update

--- app/Doctor.php
+++ app/Doctor.php
@@ -29,36 +29,37 @@ class Doctor
 
         $climate->out("\n<blue><bold>File System - FOLDERS");
 
-        $this->validate("<bold>APP</bold> as " . APP, $this->isreadable(APP));
-        $this->validate("<bold>CACHE</bold> as " . CACHE, $this->isreadable(CACHE));
-        $this->validate("<bold>DATA</bold> as " . DATA, $this->iswritable(DATA));
-        $this->validate("<bold>PARTIALS</bold> as " . PARTIALS, $this->isreadable(PARTIALS));
-        $this->validate("<bold>TEMP</bold> as " . TEMP, $this->iswritable(TEMP));
-        $this->validate("<bold>TEMPLATES</bold> as " . TEMPLATES, $this->isreadable(TEMPLATES));
-        $this->validate("<bold>WWW</bold> as " . WWW, $this->isreadable(WWW));
+        $this->validate("<bold>APP</bold> » " . APP, $this->isreadable(APP));
+        $this->validate("<bold>CACHE</bold> » " . CACHE, $this->isreadable(CACHE));
+        $this->validate("<bold>DATA</bold> » " . DATA, $this->iswritable(DATA));
+        $this->validate("<bold>PARTIALS</bold> » " . PARTIALS, $this->isreadable(PARTIALS));
+        $this->validate("<bold>TEMP</bold> » " . TEMP, $this->iswritable(TEMP));
+        $this->validate("<bold>TEMPLATES</bold> » " . TEMPLATES, $this->isreadable(TEMPLATES));
+        $this->validate("<bold>WWW</bold> » " . WWW, $this->isreadable(WWW));
 
         $climate->out("\n<blue><bold>File System - FILES");
 
-        $this->validate("<bold>CONFIG</bold> as " . CONFIG, $this->isreadable(CONFIG));
+        $this->validate("<bold>CONFIG</bold> » " . CONFIG, $this->isreadable(CONFIG));
         $this->validate("<bold>REVISIONS</bold> in ROOT", $this->isreadable(ROOT . DS . "REVISIONS"));
         $this->validate("<bold>VERSION</bold> in ROOT", $this->isreadable(ROOT . DS . "VERSION"));
+        $this->validate("<bold>.env</bold> in ROOT", $this->isreadable(".env"));
         $this->validate("<bold>router.neon</bold> in APP", $this->isreadable(APP . DS . "router.neon"));
         $this->validate("<bold>router_admin.neon</bold> in APP", $this->isreadable(APP . DS . "router_admin.neon"));
         $this->validate("<bold>router_defaults.neon</bold> in APP", $this->isreadable(APP . DS . "router_defaults.neon"));
 
-        $climate->out("\n<blue><bold>File System - WRITABLE FOLDERS");
+        $climate->out("\n<blue><bold>File System - WRITABLE");
 
+        $this->validate("<bold>ci</bold>", $this->iswritable(ROOT . DS . "ci"));
         $this->validate("<bold>CACHE</bold>", $this->iswritable(CACHE));
         $this->validate("<bold>DATA</bold>", $this->iswritable(DATA));
         $this->validate("<bold>LOGS</bold>", $this->iswritable(LOGS));
         $this->validate("<bold>TEMP</bold>", $this->iswritable(TEMP));
-        $this->validate("<bold>ci</bold>", $this->iswritable(ROOT . DS . "ci"));
 
         $climate->out("\n<blue><bold>PHP Core");
 
         $this->validate("lib <bold>curl", $this->isloaded("curl"));
         $this->validate("lib <bold>gd", $this->isloaded("gd"));
-        //$this->validate("lib <bold>imagick", $this->isloaded("imagick"));
+        $this->validate("lib <bold>imagick", $this->isloaded("imagick"));
         $this->validate("lib <bold>json", $this->isloaded("json"));
         $this->validate("lib <bold>mbstring", $this->isloaded("mbstring"));
         $this->validate("lib <bold>redis",$this->isloaded ("redis"));
