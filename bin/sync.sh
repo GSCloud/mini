b24aa4c2b0f3d085c838d758ad7e30546be6309e
Filip Oščádal <f@mxd.cz>
2021-04-13 13:59:17 +0200

    web sync

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #@author Filip Oščádal <git@gscloud.cz>
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-
 dir="$(dirname "$0")"
-. $dir"/_includes.sh"
+. "$dir/_includes.sh"
 
 if [ -z "$GLOBALSYNC" ]; then
   if [ -n "$1" ]; then export BETA="$1"; else export BETA="b"; fi
@@ -14,16 +11,29 @@ if [ -z "$GLOBALSYNC" ]; then
     export BETA=""
     info Branch: ${BETA:-MAIN}
   fi
-  sleep 3
 fi
-. "_site_cfg.sh"
 
-if [ -z "${DEST}" ]; then fail "Error in _site_cfg.sh !"; fi
-if [ -z "${HOST}" ]; then fail "Error in _site_cfg.sh !"; fi
-if [ -z "${USER}" ]; then fail "Error in _site_cfg.sh !"; fi
+if [ ! -r ".env" ]; then fail "Missing .env file!"; fi
+export $(grep -v '^#' .env | xargs -d '\n')
+
+if [ "$BETA" == "a" ]; then
+  export DEST=$DESTA
+fi
+if [ "$BETA" == "b" ]; then
+  export DEST=$DESTB
+fi
+
+[ -z "$DEST" ] && fail "Missing DEST definition!"
+[ -z "$HOST" ] && fail "Missing HOST definition!"
+[ -z "$USER" ] && fail "Missing USER definition!"
+
+info HOST: $HOST
+info USER: $USER
+info DEST: $DEST
 
 mkdir -p app ci data temp www/cdn-assets www/download www/upload
 chmod 0777 www/download www/upload >/dev/null 2>&1
+
 find www/ -type f -exec chmod 0644 {} \; >/dev/null 2>&1
 find . -type f -iname "*.sh" -exec chmod +x {} \;
 
@@ -37,13 +47,12 @@ ln -s ../. www/cdn-assets/$VERSION >/dev/null 2>&1
 info "Version: $VERSION Revisions: $REVISIONS"
 
 rsync -ahz --progress --delete-after --delay-updates --exclude "www/upload" \
+  .env \
   *.json \
   *.php \
   LICENSE \
   REVISIONS \
   VERSION \
-  _includes.sh \
-  _site_cfg.sh \
   app \
   cli.sh \
   remote_fixer.sh \
--- bin/update.sh
+++ bin/update.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #@author Filip Oščádal <git@gscloud.cz>
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-
 dir="$(dirname "$0")"
-. $dir"/_includes.sh"
+. "$dir/_includes.sh"
 
 git commit -am "web sync"
 git push origin master
@@ -16,12 +13,18 @@ echo $VERSION > VERSION
 REVISIONS=$(git rev-list --all --count)
 echo $REVISIONS > REVISIONS
 
+# clear logs and temp
 rm -rf logs/* temp/*
 ln -s ../. www/cdn-assets/$VERSION >/dev/null 2>&1
 
 info "Version: $VERSION Revisions: $REVISIONS"
 
-command -v composer >/dev/null 2>&1 || fail PHP composer is not installed!
+command -v composer >/dev/null 2>&1 || fail "PHP composer is not installed!"
+
 composer update --no-plugins --no-scripts
+
+# recalculate favicons
+cd www/img
+. ./create_favicons.sh
 
 exit 0
