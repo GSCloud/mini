2730d5b8fea0087e6f9a55a296127e1e688535d3
Filip Oščádal <f@mxd.cz>
2021-04-13 13:59:47 +0200

    web sync

--- www/img/create_favicons.sh
+++ www/img/create_favicons.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
+#@author Filip Oščádal <git@gscloud.cz>
 
-INPUT_IMAGE="$1"
-OUTPUT_DIR="$2"
+INPUT="$1"
+OUT_DIR="$2"
 
-if [ -z "$INPUT_IMAGE" ]; then INPUT_IMAGE="logo.png"; fi
-if [ -z "$OUTPUT_DIR" ]; then OUTPUT_DIR="."; fi
-if [ ! -d "$OUTPUT_DIR" ]; then
+if [ -z "$INPUT" ]; then INPUT="logo.png"; fi
+if [ -z "$OUT_DIR" ]; then OUT_DIR="."; fi
+if [ ! -d "$OUT_DIR" ]; then
   echo "ERROR: Output directory does not exist." >&2
   exit 1
 fi
 if ! [ -x "$(command -v convert)" ]; then
-  echo "ERROR: convert not found. Check if ImageMagick is installed. Get it from: https://www.imagemagick.org" >&2
+  echo "ERROR: convert not found. Check if ImageMagick is installed." >&2
   exit 1
 fi
 
-if [ -f $INPUT_IMAGE ]; then
+if [ -f $INPUT ]; then
+  convert -flatten -background none -resize 512x512 $INPUT $OUT_DIR/logo.webp
   SIZES=(16 24 29 32 40 48 57 58 60 64 70 72 76 80 87 96 114 120 128 144 150 152 167 180 192 196 256 310 320 512)
   for size in ${SIZES[@]}; do
-    convert -flatten -background none -resize ${size}x${size} $INPUT_IMAGE $OUTPUT_DIR/favicon-${size}.png
+    convert -flatten -background none -resize ${size}x${size} $INPUT $OUT_DIR/favicon-${size}.png
+    convert -flatten -background none -resize ${size}x${size} $INPUT $OUT_DIR/favicon-${size}.webp
     if [ -f favicon-${size}.png ]; then
       echo -ne "\e[0mconverting square: \e[92m$size px\e[0m\033[0K\r"
     else
-      echo "ERROR: Could not process input file $INPUT_IMAGE" >&2
+      echo "ERROR: Could not process input file $INPUT" >&2
       exit 1
     fi
   done
 else
-  echo "ERROR: Input file $INPUT_IMAGE does not exist." >&2
+  echo "ERROR: Input file $INPUT does not exist." >&2
   exit 1
 fi
