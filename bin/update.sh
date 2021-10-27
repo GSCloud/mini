b668e2872769c72ea776c44582fbe5a13af105e8
Filip Oščádal <f@mxd.cz>
2021-10-27 11:15:46 +0200

    automatic update

--- bin/update.sh
+++ bin/update.sh
@@ -4,29 +4,34 @@
 dir="$(dirname "$0")"
 . "$dir/_includes.sh"
 
-# CRLF normalization
-git add --renormalize .
-
-git commit -am "web sync"
-git push origin master
-
+# create VERSION file
 VERSION=$(git rev-parse HEAD)
 echo $VERSION > VERSION
 
+# create REVISIONS file
 REVISIONS=$(git rev-list --all --count)
 echo $REVISIONS > REVISIONS
 
-# clear logs and temp
+# clear space
 rm -rf logs/* temp/*
 ln -s ../. www/cdn-assets/$VERSION >/dev/null 2>&1
 
 info "Version: $VERSION Revisions: $REVISIONS"
 
 command -v composer >/dev/null 2>&1 || fail "PHP composer is not installed!"
-
 composer update --no-plugins --no-scripts
 
 # recalculate favicons
-#cd www/img && . ./create_favicons.sh
+cd www/img && . ./create_favicons.sh
+
+# CRLF normalization
+git add --renormalize .
+
+# add new files
+git add -A
+
+# commit changes
+git commit -am "automatic update"
+git push origin master
 
 exit 0
