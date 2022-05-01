368bd8ea5bd05646cea31e3e8cbef77cc3a46b6a
Filip Oščádal <f@mxd.cz>
2022-05-01 21:22:08 +0200

    robotic update

--- bin/create_pdf.sh
+++ bin/create_pdf.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-#@author Filip Oščádal <git@gscloud.cz>
+#@author Fred Brooker <git@gscloud.cz>
 
 dir="$(dirname "$0")"
 . "$dir/_includes.sh"
 
 command -v docker >/dev/null 2>&1 || fail "Docker is NOT installed!"
 
-# MarkDown -> ADOC
-find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.md" -exec echo "Converting {} to ADOC" \; \
-    -exec docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o "{}.adoc" \;
+# MarkDown -> ADOC on files modified in the last 24 hours
+find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -mtime -1 -iname "*.md" \
+    -exec echo "Converting {} to ADOC" \; \
+    -exec docker run --rm -v "$(pwd)":/data pandoc/core:latest -f markdown -t asciidoc -i {} -o "{}.adoc" \;
 
-# ADOC -> PDF
-find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.adoc" -exec echo "Converting {} to PDF" \; \
-    -exec docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/documents/ asciidoctor/docker-asciidoctor:1.9.0 asciidoctor-pdf "{}" \;
+# ADOC -> PDF on files modified in the last 24 hours
+find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -mtime -1 -iname "*.adoc" \
+    -exec echo "Converting {} to PDF" \; \
+    -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor:latest asciidoctor-pdf "{}" \;
 
 # cleaning
 find . -maxdepth 1 -iname "*.adoc" -delete
 rm temp/* >/dev/null 2>&1
 
 # phpdocumentor
-docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/data phpdoc/phpdoc:3.1.0 \
+docker run --rm -v $(pwd):/data phpdoc/phpdoc:3.1.0 \
     run -d . -t ./doc --ignore "vendor/"
 
 # make link to documentation
